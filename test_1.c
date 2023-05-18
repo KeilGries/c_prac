9cd1c8693d5ed6ead826dfa4f7cc9fe1be7040b1
KG <KeilGriesLP@gmail.com>
2023-05-18 13:32:38 -0400

    Compiled program to an exe file. Added comments and multiline comments. Added tab spacing.

--- test_1.c
+++ test_1.c
@@ -2,8 +2,18 @@
 
 int main(){
 
-    printf("Suh World!?\n");
-    printf("Suh Ma Drilla");
+    // This is a comment
+
+    /*
+    This
+    is
+    a
+    multiline
+    comment
+    */
+    printf("1\t2\t3\t4\t5\n"); // Use \t to add a tab space
+    printf("Suh World!?\n"); // Greet the world
+    printf("Suh Ma Drilla"); // Greet a fellow drilla
     return 0;
 
 }
