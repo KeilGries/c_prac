64e1c82863b52751ab56df30e4b3b805f7a0630f
KG <KeilGriesLP@gmail.com>
2023-05-18 14:46:15 -0400

    Did some work with declaring and initializing different types of variables.

---
+++ test_1_getting_started.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    // This is a comment
+
+    /*
+    This
+    is
+    a
+    multiline
+    comment
+    */
+   
+    printf("1\t2\t3\t4\t5\n"); // Use \t to add a tab space
+    printf("Suh World!?\n"); // Greet the world
+    printf("Suh Ma Drilla"); // Greet a fellow drilla
+    return 0;
+
+}
